70d7d8fde824764f6fce889b749f57e71d117ef5
mouhsinandroid <mouhsinbourqaiba@gmail.com>
2020-08-29 17:05:02 +0200

    Initial commit

---
+++ app/src/main/java/com/mouhsinbourqaiba/android/gadsleadersboard/model/LearningLeader.kt
@@ -0,0 +1,8 @@
+package com.mouhsinbourqaiba.android.gadsleadersboard.model
+
+data class LearningLeader (
+    val name: String?,
+    val score: Int?,
+    val country: String?,
+    val badgeUrl: String?
+)
---
+++ app/src/main/java/com/mouhsinbourqaiba/android/gadsleadersboard/model/SkillIqLeader.kt
@@ -0,0 +1,8 @@
+package com.mouhsinbourqaiba.android.gadsleadersboard.model
+
+data class SkillIqLeader (
+    val name: String?,
+    val hours: Int?,
+    val country: String?,
+    val badgeUrl: String?
+)
