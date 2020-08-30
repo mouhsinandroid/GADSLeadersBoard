b75707535a6b1f48e9291c14eaef4e43f6a01f56
mouhsinandroid <mouhsinbourqaiba@gmail.com>
2020-08-30 19:42:20 +0200

    feat: - add model class : LearningLeader , SkillIqLeader       - add item_learning_leader       - add item_skill_iq_leader.xml       - add LearnerListAdapter

---
+++ app/src/main/java/com/mouhsinbourqaiba/android/gadsleadersboard/view/home/learner/LearnerListAdapter.kt
@@ -0,0 +1,29 @@
+package com.mouhsinbourqaiba.android.gadsleadersboard.view.home.learner
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.mouhsinbourqaiba.android.gadsleadersboard.R
+import com.mouhsinbourqaiba.android.gadsleadersboard.databinding.ItemLearningLeaderBinding
+import com.mouhsinbourqaiba.android.gadsleadersboard.model.LearningLeader
+
+class LearnerListAdapter(private val learnerList: ArrayList<LearningLeader>): RecyclerView.Adapter<LearnerListAdapter.LearnerViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): LearnerViewHolder {
+        val inflater = LayoutInflater.from(parent.context)
+        val binding = DataBindingUtil.inflate<ItemLearningLeaderBinding>(inflater, R.layout.item_learning_leader, parent, false)
+
+        return LearnerViewHolder(binding)
+    }
+
+    override fun getItemCount() = learnerList.size
+
+    override fun onBindViewHolder(holder: LearnerViewHolder, position: Int) {
+        holder.view.learner = learnerList[position]
+    }
+
+    class LearnerViewHolder(var view: ItemLearningLeaderBinding): RecyclerView.ViewHolder(view.root)
+
+
+}
