182ea096c049d459009d023600e8004148c2230f
mouhsinandroid <mouhsinbourqaiba@gmail.com>
2020-09-07 18:36:26 +0200

    learners workflow : - Create learner recyclerview - Create viewModel - get data from server - display data in recyclerview

--- app/src/main/java/com/mouhsinbourqaiba/android/gadsleadersboard/model/LearningLeader.kt
+++ app/src/main/java/com/mouhsinbourqaiba/android/gadsleadersboard/model/LearningLeader.kt
@@ -2,7 +2,7 @@ package com.mouhsinbourqaiba.android.gadsleadersboard.model
 
 data class LearningLeader (
     val name: String?,
-    val score: Int?,
+    val hours: Int?,
     val country: String?,
     val badgeUrl: String?
 )
---
+++ app/src/main/java/com/mouhsinbourqaiba/android/gadsleadersboard/util/Utils.kt
@@ -0,0 +1,43 @@
+package com.mouhsinbourqaiba.android.gadsleadersboard.util
+
+import android.content.Context
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.databinding.BindingAdapter
+import androidx.swiperefreshlayout.widget.CircularProgressDrawable
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
+import com.mouhsinbourqaiba.android.gadsleadersboard.R
+
+fun getProgressDrawable(context: Context): CircularProgressDrawable {
+    return CircularProgressDrawable(context).apply {
+        strokeWidth = 10f
+        centerRadius = 50f
+        start()
+    }
+}
+
+fun ImageView.loadImage(uri: String?, progressDrawable: CircularProgressDrawable) {
+    val options = RequestOptions()
+        .placeholder(progressDrawable)
+        .error(R.mipmap.ic_launcher)
+
+    Glide.with(context)
+        .setDefaultRequestOptions(options)
+        .load(uri)
+        .into(this)
+
+}
+
+@BindingAdapter("android:imageUrl")
+fun loadImage(view: ImageView, url: String) {
+    view.loadImage(url, getProgressDrawable(view.context))
+}
+
+@BindingAdapter(value = ["hours", "country"], requireAll = false)
+fun descLearner(view: TextView, hours: Int, country: String) {
+    view.setText("$hours learning hours, $country.")
+}
+
+
+
